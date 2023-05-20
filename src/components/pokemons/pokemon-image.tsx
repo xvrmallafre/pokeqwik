7752e4de031f7bdda285f96ffb0222e5aada87c6
Xavier Mallafré <xvrmallafre@gmail.com>
2023-05-20 13:10:43 +0200

    added a new button to allow the user to do the image dark or reveal the pokemon

--- src/components/pokemons/pokemon-image.tsx
+++ src/components/pokemons/pokemon-image.tsx
@@ -4,17 +4,16 @@ interface Props {
     id: number;
     size?: number;
     backImage: boolean;
+    isVisible?: boolean;
 }
 
-export const PokemonImage = component$(({ id, size = 200, backImage = false }: Props) => {
+export const PokemonImage = component$(({ id, size = 200, backImage = false, isVisible = true }: Props) => {
     const imageLoaded = useSignal(false);
     useTask$(({ track }) => {
         track(() => id);
 
         imageLoaded.value = false;
     });
-
-
 
     const url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
     const image = backImage ? `${url}back/${id}.png` : `${url}${id}.png`;
@@ -29,10 +28,10 @@ export const PokemonImage = component$(({ id, size = 200, backImage = false }: P
                 alt="Pokemon sprite"
                 style={{ width: `${size}px` }}
                 onLoad$={() => imageLoaded.value = true}
-                class={{
+                class={[{
                     'hidden': !imageLoaded.value,
-                    'brightness-0': true
-                }}
+                    'brightness-0': !isVisible,
+                }, 'transition-all']}
             />
         </div>
     )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ export default component$(() => {
 
   const pokemonId = useSignal(1);
   const pokemonImage = useSignal(false);
+  const pokemonVisibility = useSignal(true);
 
   const changePokemonId = $((value: number) => {
     if (pokemonId.value + value <= 0) return;
@@ -18,26 +19,28 @@ export default component$(() => {
     pokemonImage.value = !pokemonImage.value;
   });
 
+  const toggleVisibility = $(() => {
+    pokemonVisibility.value = !pokemonVisibility.value;
+
+    const button = document.querySelector('.change-visibility');
+    if (button) {
+      button.innerHTML = pokemonVisibility.value ? 'Oscurecer' : 'Revelar';
+    }
+  });
+
   return (
     // pokemonId devuelve el valor de la señal
     // pokemonId.value devuelve el valor de la señal
-
     // ambos son equivalentes ya que si no se le especifica un método a la señal, devuelve el .value
     <>
-
       <span class="text-2xl">Buscador Simple</span>
-      {/* <span class="text-9xl">{pokemonId}</span> */}
-
-      <PokemonImage id={pokemonId.value} backImage={pokemonImage.value} />
-
-
+      <PokemonImage id={pokemonId.value} backImage={pokemonImage.value} isVisible={pokemonVisibility.value} />
       <div class="mt-2">
         <button onClick$={() => changePokemonId(-1)} class="btn btn-primary mr-2">Anterior</button>
         <button onClick$={() => changePokemonId(+1)} class="btn btn-primary mr-2">Siguiente</button>
-        <button onClick$={() => changePokemonImage()} class="btn btn-primary">Voltear</button>
+        <button onClick$={() => changePokemonImage()} class="btn btn-primary mr-2">Voltear</button>
+        <button onClick$={() => toggleVisibility()} class="btn btn-primary w-[120px] change-visibility">Oscurecer</button>
       </div>
-
-
     </>
   );
 });
